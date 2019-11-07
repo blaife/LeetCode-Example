f4b535e832f8a7f8dbfc6c09da4648508bd40319
blaife <2767026270@qq.com>
2019-11-07 15:20:38 +0800

    Initial commit

---
+++ src/leetcode/array/t004medianoftwosortedarrays/Solution.java
@@ -0,0 +1,168 @@
+package leetcode.array.t004medianoftwosortedarrays;
+
+import com.sun.deploy.util.ArrayUtil;
+
+/**
+ * """""""""""""""""""寻找两个有序数组的中位数"""""""""""""""""""
+ * 题意：
+ * 给定两个大小为 m 和 n 的有序数组 nums1 和 nums2。
+ * 请你找出这两个有序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
+ * 你可以假设 nums1 和 nums2 不会同时为空。
+ *
+ * 示例1：
+ * nums1 = [1, 3]
+ * nums2 = [2]
+ * 则中位数是 2.0
+ *
+ * 示例1：
+ * nums1 = [1, 2]
+ * nums2 = [3, 4]
+ * 则中位数是 (2 + 3)/2 = 2.5
+ */
+
+
+public class Solution {
+
+    /**
+     * 解法一：简单的合并排序后取中位数 -- 性能极差  未达到要求
+     * @param nums1
+     * @param nums2
+     * @return
+     */
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int[] num = new int[nums1.length + nums2.length];
+        System.arraycopy(nums1, 0, num, 0, nums1.length);
+        System.arraycopy(nums2, 0, num, nums1.length, nums2.length);
+        for (int i = 0; i < num.length-1; i++){
+            for (int j = i+1; j < num.length; j++){
+                int param = num[i];
+                if (param > num[j]) {
+                    num[i] = num[j];
+                    num[j] = param;
+                }
+            }
+        }
+
+        if (num.length%2 != 0){
+            return num[num.length/2];
+        } else {
+            return (Double.valueOf(num[num.length/2])+Double.valueOf(num[num.length/2-1]))/2;
+        }
+    }
+
+    /**
+     * 两个数组从头取值判断，到达中位数位置后停止 未达到要求
+     * @param A
+     * @param B
+     * @return
+     */
+    public double findMedianSortedArrays2(int[] A, int[] B) {
+        int m = A.length;
+        int n = B.length;
+        int len = m + n;
+        int left = -1, right = -1;
+        int aStart = 0, bStart = 0;
+        for (int i = 0; i <= len / 2; i++) {
+            left = right;
+            if (aStart < m && (bStart >= n || A[aStart] < B[bStart])) {
+                right = A[aStart++];
+            } else {
+                right = B[bStart++];
+            }
+        }
+        if ((len & 1) == 0){
+            return (left + right) / 2.0;
+        } else {
+            return right;
+        }
+    }
+
+    /**
+     * 解法3：取中间数的一半对比之后去除
+     * @param nums1
+     * @param nums2
+     * @return
+     */
+    public double findMedianSortedArrays3(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        int m = nums2.length;
+        int left = (n + m + 1) / 2;
+        int right = (n + m + 2) / 2;
+        //将偶数和奇数的情况合并，如果是奇数，会求两次同样的 k 。
+        return (getKth(nums1, 0, n - 1, nums2, 0, m - 1, left) + getKth(nums1, 0, n - 1, nums2, 0, m - 1, right)) * 0.5;
+    }
+
+    private int getKth(int[] nums1, int start1, int end1, int[] nums2, int start2, int end2, int k) {
+        int len1 = end1 - start1 + 1;
+        int len2 = end2 - start2 + 1;
+        //让 len1 的长度小于 len2，这样就能保证如果有数组空了，一定是 len1
+        if (len1 > len2) return getKth(nums2, start2, end2, nums1, start1, end1, k);
+        if (len1 == 0) return nums2[start2 + k - 1];
+
+        if (k == 1) return Math.min(nums1[start1], nums2[start2]);
+
+        int i = start1 + Math.min(len1, k / 2) - 1;
+        int j = start2 + Math.min(len2, k / 2) - 1;
+
+        if (nums1[i] > nums2[j]) {
+            return getKth(nums1, start1, end1, nums2, j + 1, end2, k - (j - start2 + 1));
+        }
+        else {
+            return getKth(nums1, i + 1, end1, nums2, start2, end2, k - (i - start1 + 1));
+        }
+    }
+
+    /**
+     * 解法4：二分法，分治算法，尚未理解
+     * @param A
+     * @param B
+     * @return
+     */
+    public double findMedianSortedArrays4(int[] A, int[] B) {
+        int m = A.length;
+        int n = B.length;
+        if (m > n) {
+            return findMedianSortedArrays(B, A); // 保证 m <= n
+        }
+        int iMin = 0, iMax = m;
+        while (iMin <= iMax) {
+            int i = (iMin + iMax) / 2;
+            int j = (m + n + 1) / 2 - i;
+            if (j != 0 && i != m && B[j - 1] > A[i]) { // i 需要增大
+                iMin = i + 1;
+            } else if (i != 0 && j != n && A[i - 1] > B[j]) { // i 需要减小
+                iMax = i - 1;
+            } else { // 达到要求，并且将边界条件列出来单独考虑
+                int maxLeft = 0;
+                if (i == 0) {
+                    maxLeft = B[j - 1];
+                } else if (j == 0) {
+                    maxLeft = A[i - 1];
+                } else {
+                    maxLeft = Math.max(A[i - 1], B[j - 1]);
+                }
+                if ((m + n) % 2 == 1) {
+                    return maxLeft;
+                } // 奇数的话不需要考虑右半部分
+
+                int minRight = 0;
+                if (i == m) {
+                    minRight = B[j];
+                } else if (j == n) {
+                    minRight = A[i];
+                } else {
+                    minRight = Math.min(B[j], A[i]);
+                }
+
+                return (maxLeft + minRight) / 2.0; //如果是偶数的话返回结果
+            }
+        }
+        return 0.0;
+    }
+
+    public static void main(String[] args) {
+        int[] num1 = new int[]{1,2,3,4,5,6,7};
+        int[] num2 = new int[]{2};
+        System.out.println(new Solution().findMedianSortedArrays2(num1, num2));
+    }
+}
---
+++ src/leetcode/array/t011containerwithmostwater/Solution.java
@@ -0,0 +1,40 @@
+package leetcode.array.t011containerwithmostwater;
+
+/**
+ * """"""""""""""""""""盛最多水的容器""""""""""""""""""""
+ * 题意：
+ * 给定 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。
+ * 在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0)。
+ * 找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+ * 说明：你不能倾斜容器，且 n 的值至少为 2。
+ *
+ * 示例:
+ * 输入: [1,8,6,2,5,4,8,3,7]
+ * 输出: 49
+ */
+public class Solution {
+    /**
+     * 使用双指针，一次循环。消除不可能为最大值的参数
+     * @param height
+     * @return
+     */
+    public int maxArea(int[] height) {
+        int i = 0;
+        int j = height.length-1;
+        int area = 0;
+        while (i < j){
+            area = Math.max(area, Math.max(height[i], height[j]) * (j-i));
+            if (height[i] > height[j]) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+        return area;
+    }
+
+    public static void main(String[] args) {
+        int[] height = new int[]{1,3,5,6,3,4,4,5,7,8};
+        new Solution().maxArea(height);
+    }
+}
