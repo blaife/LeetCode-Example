6f93bbdd821e12fe9511eeddbf53f72a6e4bcb2b
blaife <2767026270@qq.com>
2019-11-09 23:16:01 +0800

    相邻判断--下一个排序

---
+++ src/leetcode/array/t031nextpermutation/Solution.java
@@ -0,0 +1,63 @@
+package leetcode.array.t031nextpermutation;
+
+import java.util.Arrays;
+
+/**
+ * """""""""""""""""""""""""""下一个排列"""""""""""""""""""""""""""
+ * 题意：
+ * 实现获取下一个排列的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列。
+ * 如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。
+ * 必须原地修改，只允许使用额外常数空间。
+ *
+ * 以下是一些例子，输入位于左侧列，其相应输出位于右侧列。
+ * 1,2,3 → 1,3,2
+ * 3,2,1 → 1,2,3
+ * 1,1,5 → 1,5,1
+ */
+public class Solution {
+    /**
+     * 思路：使用倒序相邻判断
+     * @param nums
+     */
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i + 1] <= nums[i]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+    }
+
+    /**
+     * 一段内容更换位置
+     * @param nums
+     * @param start
+     */
+    private void reverse(int[] nums, int start) {
+        int i = start, j = nums.length - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    /**
+     * 更换数组中两个下标中的内容
+     * @param nums
+     * @param i
+     * @param j
+     */
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+}
