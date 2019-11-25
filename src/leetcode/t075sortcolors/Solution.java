ddd0668280c7f99c094c45fb6c28b9ea67b461ae
blaife <2767026270@qq.com>
2019-11-25 13:45:14 +0800

    双指针，原位换值 -- 颜色分类

--- src/leetcode/t075sortcolors/Solution.java
+++ src/leetcode/t075sortcolors/Solution.java
@@ -23,15 +23,16 @@ public class Solution {
     public void sortColors(int[] nums) {
         int left = 0;
         int right = nums.length-1;
+        int temp;
         for (int i = 0; i <= right; i++) {
             while (nums[i] == 2 && i < right) {
-                int temp = nums[i];
+                temp = nums[i];
                 nums[i] = nums[right];
                 nums[right] = temp;
                 right--;
             }
             while (nums[i] == 0 && i > left) {
-                int temp = nums[i];
+                temp = nums[i];
                 nums[i] = nums[left];
                 nums[left] = temp;
                 left++;
