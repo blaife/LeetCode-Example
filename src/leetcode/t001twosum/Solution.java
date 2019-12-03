0f2ed521cdbe87974f7a146ee7d49dc0783d6e58
blaife <2767026270@qq.com>
2019-12-03 14:05:05 +0800

    双层循环穷举法 -- 两数之和

--- src/leetcode/t001twosum/Solution.java
+++ src/leetcode/t001twosum/Solution.java
@@ -3,7 +3,7 @@ package leetcode.t001twosum;
 /**
  * """""""""""""""""""""""""""""两数之和"""""""""""""""""""""""""""""
  * 题意：
- * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那两个整数，并返回他们的数组下标。
  * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
  *
  * 示例:
@@ -13,6 +13,7 @@ package leetcode.t001twosum;
  */
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
+        // 双层循环穷举法
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[j] == target - nums[i]) {
@@ -20,6 +21,7 @@ public class Solution {
                 }
             }
         }
+        // 没有匹配值 抛出错误信息
         throw new IllegalArgumentException("No two sum solution");
     }
 
