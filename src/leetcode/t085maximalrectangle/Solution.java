d3dff3a795eaef0e5001e543c78221982ec7fadd
blaife <2767026270@qq.com>
2019-12-04 15:01:15 +0800

    动态规划 -- 最大矩形

--- src/leetcode/t085maximalrectangle/Solution.java
+++ src/leetcode/t085maximalrectangle/Solution.java
@@ -96,21 +96,26 @@ public class Solution {
             int cur_left = 0, cur_right = n;
             // update height
             for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == '1') height[j]++;
-                else height[j] = 0;
+                if (matrix[i][j] == '1') {
+                    height[j]++;
+                } else {
+                    height[j] = 0;
+                }
             }
             // update left
             for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == '1') left[j] = Math.max(left[j], cur_left);
-                else {
+                if (matrix[i][j] == '1') {
+                    left[j] = Math.max(left[j], cur_left);
+                } else {
                     left[j] = 0;
                     cur_left = j + 1;
                 }
             }
             // update right
             for (int j = n - 1; j >= 0; j--) {
-                if (matrix[i][j] == '1') right[j] = Math.min(right[j], cur_right);
-                else {
+                if (matrix[i][j] == '1') {
+                    right[j] = Math.min(right[j], cur_right);
+                } else {
                     right[j] = n;
                     cur_right = j;
                 }
