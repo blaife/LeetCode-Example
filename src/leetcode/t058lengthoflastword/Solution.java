e1f69de691e82f047e88df55b9a19a88f3f0e4a6
BigBlackKnife <2767026270@qq.com>
2020-02-12 18:56:27 +0800

    关键的空格（循环） -- 最后一个单词的长度

--- src/leetcode/t058lengthoflastword/Solution.java
+++ src/leetcode/t058lengthoflastword/Solution.java
@@ -2,6 +2,17 @@ package leetcode.t058lengthoflastword;
 
 /**
  * """"""""""""""""""""""""""""""""""""""""""""""最后一个单词的长度""""""""""""""""""""""""""""""""""""""""""""""
+ *
+ * 给定一个仅包含大小写字母和空格 ' ' 的字符串 s，返回其最后一个单词的长度。
+ * 如果字符串从左向右滚动显示，那么最后一个单词就是最后出现的单词。
+ * 如果不存在最后一个单词，请返回 0 。
+ * 说明：一个单词是指仅由字母组成、不包含任何空格的 最大子字符串。
+ *
+ *
+ * 示例:
+ * 输入: "Hello World"
+ * 输出: 5
+ *
  */
 public class Solution {
 
@@ -13,11 +24,9 @@ public class Solution {
     public int lengthOfLastWord(String s) {
         int from = s.length()-1; // 起始点
         int left = s.lastIndexOf(" ", from);
-        while (left == from){
+        while (left == from && left > 0){
             from--;
             left = s.lastIndexOf(" ", from);
-            System.out.println(left);
-            System.out.println(from);
         }
         int result = from-left;
         if (result < 0) {
