4660535614dc596e903b2e103463f469f824d45a
BigBlackKnife <2767026270@qq.com>
2020-02-15 18:23:36 +0800

    动态规划/递归，暴力 -- 爬楼梯

---
+++ src/leetcode/t070climbingstairs/Solution.java
@@ -0,0 +1,67 @@
+package leetcode.t070climbingstairs;
+
+/**
+ * """"""""""""""""""""""""""""""""""""爬楼梯""""""""""""""""""""""""""""""""""""
+ * 题意：
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ * 注意：给定 n 是一个正整数。
+ *
+ * 示例 1：
+ * 输入： 2
+ * 输出： 2
+ * 解释： 有两种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶
+ * 2.  2 阶
+ *
+ * 示例 2：
+ * 输入： 3
+ * 输出： 3
+ * 解释： 有三种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶 + 1 阶
+ * 2.  1 阶 + 2 阶
+ * 3.  2 阶 + 1 阶
+ */
+public class Solution {
+    /**
+     * 爬楼梯
+     * @param n 楼梯台阶数
+     * @return
+     */
+    public int climbStairs(int n) {
+        // 动态规划
+        if (n == 1) {
+            return 1;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+        // return climb_Stairs(0, n);
+    }
+
+    /**
+     * 递归算法（暴力, 超时）
+      * @param i 当前台阶数
+     * @param n 总台阶数
+     * @return
+     */
+    public int climb_Stairs(int i, int n) {
+        if (i > n) {
+            return 0;
+        }
+        if (i == n) {
+            return 1;
+        }
+        return climb_Stairs(i + 1, n) + climb_Stairs(i + 2, n);
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().climbStairs(44));
+    }
+
+}
