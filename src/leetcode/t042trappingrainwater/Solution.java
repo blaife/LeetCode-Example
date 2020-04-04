36aa40500cc78c5f918d07926909a4d8fac23014
BigBlackKnife <2767026270@qq.com>
2020-04-04 12:55:51 +0800

    双指针 - 接雨水

--- src/leetcode/t042trappingrainwater/Solution.java
+++ src/leetcode/t042trappingrainwater/Solution.java
@@ -20,28 +20,35 @@ package leetcode.t042trappingrainwater;
 public class Solution {
     /**
      * 双指针求解
-     * @param height
-     * @return
+     * @param height 柱高集合
+     * @return 最大雨水量
      */
     public int trap(int[] height) {
         int sum = 0;
         int max_left = 0;
         int max_right = 0;
+        // 左右指针，均避开第一位
         int left = 1;
-        int right = height.length - 2; // 加右指针进去
+        int right = height.length - 2;
         for (int i = 1; i < height.length - 1; i++) {
-            //从左到右更
+            //从左到右更， 即左边低
             if (height[left - 1] < height[right + 1]) {
+                // 比较当前指向的点 左边的最大高度
                 max_left = Math.max(max_left, height[left - 1]);
+                // min是当前指向值左右的最小高度
                 int min = max_left;
+                // 最小值都高于当前指向值，则增加水量
                 if (min > height[left]) {
                     sum = sum + (min - height[left]);
                 }
                 left++;
-                //从右到左更
+                //从右到左更，即右边低
             } else {
+                // 比较当前指向的点 右边的最大高度
                 max_right = Math.max(max_right, height[right + 1]);
+                // min是当前指向值左右的最小高度
                 int min = max_right;
+                // 最小值都高于当前指向值，则增加水量
                 if (min > height[right]) {
                     sum = sum + (min - height[right]);
                 }
