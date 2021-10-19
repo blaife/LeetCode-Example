916d0bdcf91b56006deb14f8109678bd78a6ed73
blaife <2767026270@qq.com>
2021-10-19 10:24:37 +0800

    单词查找树 - 添加与搜索单词，数据结构设计

---
+++ src/leetcode/t211designaddandsearchwordsdatastructure/Trie.java
@@ -0,0 +1,37 @@
+package leetcode.t211designaddandsearchwordsdatastructure;
+
+/**
+ * @Description: 单词查找树
+ * @Author: magd39318
+ * @Date: 2021/10/19 10:10
+ */
+public class Trie {
+    private Trie[] children;
+    private boolean isEnd;
+
+    public Trie() {
+        children = new Trie[26];
+        isEnd = false;
+    }
+
+    public void insert(String word) {
+        Trie node = this;
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            int index = ch - 'a';
+            if (node.children[index] == null) {
+                node.children[index] = new Trie();
+            }
+            node = node.children[index];
+        }
+        node.isEnd = true;
+    }
+
+    public Trie[] getChildren() {
+        return children;
+    }
+
+    public boolean isEnd() {
+        return isEnd;
+    }
+}
---
+++ src/leetcode/t211designaddandsearchwordsdatastructure/WordDictionary.java
@@ -0,0 +1,49 @@
+package leetcode.t211designaddandsearchwordsdatastructure;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @Description: 字典对象
+ * @Author: magd39318
+ * @Date: 2021/10/19 9:54
+ */
+public class WordDictionary {
+
+    private Trie root;
+
+    public WordDictionary() {
+        root = new Trie();
+    }
+
+    public void addWord(String word) {
+        root.insert(word);
+    }
+
+    public boolean search(String word) {
+        return dfs(word, 0, root);
+    }
+
+    private boolean dfs(String word, int index, Trie node) {
+        if (index == word.length()) {
+            return node.isEnd();
+        }
+        char ch = word.charAt(index);
+        if (Character.isLetter(ch)) {
+            int childIndex = ch - 'a';
+            Trie child = node.getChildren()[childIndex];
+            if (child != null && dfs(word, index + 1, child)) {
+                return true;
+            }
+        } else {
+            for (int i = 0; i < 26; i++) {
+                Trie child = node.getChildren()[i];
+                if (child != null && dfs(word, index + 1, child)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+
